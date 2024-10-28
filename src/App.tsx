4daa69848baf7f05a6d7568df7e365527eb67758
Vadym Babisov <vadim.babisov@gmail.com>
2024-10-28 20:16:35 +0200

    fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,7 @@ const App = () => {
   return (
     <div className="lg:grid lg:grid-cols-3">
       <div
-        className="hidden lg:block"
-        style={{
+          style={{
           background: 'linear-gradient(339.02deg, #0D3251 0%, #19476C 103.05%)',
           display: location.pathname.includes('/onboard-complete') || location.pathname.includes('/login-success') ? 'none' : 'block',  
         }}
--- src/components/VerticalBar.tsx
+++ src/components/VerticalBar.tsx
@@ -40,104 +40,106 @@ const VerticalBar = () => {
 
   return (
     <>
-      <div
-        className="w-[331px] grid justify-start"
-        style={{ marginInline: paddingLeft }}
-      >
-        <div className="flex flex-col items-center pt-[204px]">
-          <div className="flex items-center">
-            <div
-              className="w-8 h-8 rounded-full border-2 relative"
-              style={{
-                borderColor: registrationStep >= 1 ? '#32ABF2' : '#5D7FA3',
-                backgroundColor:
-                  registrationStep >= 2 ? '#32ABF2' : 'transparent',
-                backgroundImage:
-                  registrationStep >= 2 ? `url(${Checkmark})` : '',
-                backgroundPosition: registrationStep >= 2 ? 'center' : '',
-                backgroundRepeat: registrationStep >= 2 ? 'no-repeat' : '',
-              }}
-            />
-            <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
-              Welcome
-            </span>
-          </div>
+      <div className='hidden lg:block'>
+        <div
+          className="w-[331px] grid justify-start"
+          style={{ marginInline: paddingLeft }}
+        >
+          <div className="flex flex-col items-center pt-[204px]">
+            <div className="flex items-center">
+              <div
+                className="w-8 h-8 rounded-full border-2 relative"
+                style={{
+                  borderColor: registrationStep >= 1 ? '#32ABF2' : '#5D7FA3',
+                  backgroundColor:
+                    registrationStep >= 2 ? '#32ABF2' : 'transparent',
+                  backgroundImage:
+                    registrationStep >= 2 ? `url(${Checkmark})` : '',
+                  backgroundPosition: registrationStep >= 2 ? 'center' : '',
+                  backgroundRepeat: registrationStep >= 2 ? 'no-repeat' : '',
+                }}
+              />
+              <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
+                Welcome
+              </span>
+            </div>
 
-          <div
-            className="w-0 h-12 border-2"
-            style={{
-              borderColor: registrationStep >= 2 ? '#32ABF2' : '#5D7FA3',
-            }}
-          />
-
-          <div className="flex items-center">
             <div
-              className="w-8 h-8 rounded-full border-2 relative"
+              className="w-0 h-12 border-2"
               style={{
                 borderColor: registrationStep >= 2 ? '#32ABF2' : '#5D7FA3',
-                backgroundColor:
-                  registrationStep >= 3 ||
-                  currentURL.includes('/shopify-success')
-                    ? '#32ABF2'
-                    : 'transparent',
-                backgroundImage:
-                  registrationStep >= 3 ||
-                  currentURL.includes('/shopify-success')
-                    ? `url(${Checkmark})`
-                    : '',
-                backgroundPosition:
-                  registrationStep >= 3 ||
-                  currentURL.includes('/shopify-success')
-                    ? 'center'
-                    : '',
-                backgroundRepeat:
-                  registrationStep >= 3 ||
-                  currentURL.includes('/shopify-success')
-                    ? 'no-repeat'
-                    : '',
               }}
             />
-            <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
-              Connect your Shopify store
-            </span>
-          </div>
 
-          <div
-            className="w-0 h-12 border-2"
-            style={{
-              borderColor: registrationStep >= 3 ? '#32ABF2' : '#5D7FA3',
-            }}
-          />
+            <div className="flex items-center">
+              <div
+                className="w-8 h-8 rounded-full border-2 relative"
+                style={{
+                  borderColor: registrationStep >= 2 ? '#32ABF2' : '#5D7FA3',
+                  backgroundColor:
+                    registrationStep >= 3 ||
+                    currentURL.includes('/shopify-success')
+                      ? '#32ABF2'
+                      : 'transparent',
+                  backgroundImage:
+                    registrationStep >= 3 ||
+                    currentURL.includes('/shopify-success')
+                      ? `url(${Checkmark})`
+                      : '',
+                  backgroundPosition:
+                    registrationStep >= 3 ||
+                    currentURL.includes('/shopify-success')
+                      ? 'center'
+                      : '',
+                  backgroundRepeat:
+                    registrationStep >= 3 ||
+                    currentURL.includes('/shopify-success')
+                      ? 'no-repeat'
+                      : '',
+                }}
+              />
+              <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
+                Connect your Shopify store
+              </span>
+            </div>
 
-          <div className="flex items-center">
             <div
-              className="w-8 h-8 bg-transparent rounded-full border-2 relative"
+              className="w-0 h-12 border-2"
               style={{
                 borderColor: registrationStep >= 3 ? '#32ABF2' : '#5D7FA3',
               }}
             />
-            <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
-              Connect your customer support email
-            </span>
-          </div>
 
-          <div
-            className="w-0 h-12 border-2"
-            style={{
-              borderColor: '#5D7FA3',
-            }}
-          />
+            <div className="flex items-center">
+              <div
+                className="w-8 h-8 bg-transparent rounded-full border-2 relative"
+                style={{
+                  borderColor: registrationStep >= 3 ? '#32ABF2' : '#5D7FA3',
+                }}
+              />
+              <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
+                Connect your customer support email
+              </span>
+            </div>
 
-          <div className="flex items-center">
             <div
-              className="w-8 h-8 bg-transparent rounded-full border-2 relative"
+              className="w-0 h-12 border-2"
               style={{
                 borderColor: '#5D7FA3',
               }}
             />
-            <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
-              Done
-            </span>
+
+            <div className="flex items-center">
+              <div
+                className="w-8 h-8 bg-transparent rounded-full border-2 relative"
+                style={{
+                  borderColor: '#5D7FA3',
+                }}
+              />
+              <span className="ml-12 text-base absolute font-inter font-medium text-[16px] leading-[24px] tracking-[-0.01em] text-left text-white">
+                Done
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -179,7 +181,7 @@ const VerticalBar = () => {
         </div>
       )}
 
-      <div className="fixed bottom-0 left-0 w-1/3 h-[100px] mb-14">
+      <div className="fixed bottom-0 left-0 w-1/3 h-[100px] mb-14 hidden lg:block">
         <div
           className="bg-[#134267] p-4 flex items-center gap-4 rounded-[8px]"
           style={{ marginLeft: paddingLeft, marginRight: paddingLeft }}
